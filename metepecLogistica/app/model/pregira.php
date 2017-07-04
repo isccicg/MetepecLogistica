063c310d79b01c440c04950bc2d28a508fb9eac5
ING.RAFAEL <ING.RAFAEL@192.168.3.10>
2017-07-03 22:37:41 -0500

    Se escapan los datos

--- metepecLogistica/app/model/pregira.php
+++ metepecLogistica/app/model/pregira.php
@@ -13,6 +13,39 @@ class gira
 	}
 	public function altaPreGira($datos)
 	{
-
+		$citatorioId = mysql_real_escape_string(strip_tags(stripslashes(trim(1))));
+		$fecha = date("Y-m-d",strtotime(mysql_real_escape_string(strip_tags(stripslashes(trim($datos["fecha"]))))));
+		$hora = mysql_real_escape_string(strip_tags(stripslashes(trim($datos["hora"]))));
+		$nomEvento = mysql_real_escape_string(strip_tags(stripslashes(trim($datos["evento"]))));
+		$responsablepG = mysql_real_escape_string(strip_tags(stripslashes(trim($datos["responsablepG"]))));
+		$responsable = mysql_real_escape_string(strip_tags(stripslashes(trim($datos["responsable"]))));
+		$cargoResponsable = mysql_real_escape_string(strip_tags(stripslashes(trim($datos["cargoResponsable"]))));
+		$emailResponsable = mysql_real_escape_string(strip_tags(stripslashes(trim($datos["emailResponsable"]))));
+		$telefonoResponsable = mysql_real_escape_string(strip_tags(stripslashes(trim($datos["telefonoResponsable"]))));
+		$enlace = mysql_real_escape_string(strip_tags(stripslashes(trim($datos["enlace"]))));
+		$cargoEnlace = mysql_real_escape_string(strip_tags(stripslashes(trim($datos["cargoEnlace"]))));
+		$emailEnlace = mysql_real_escape_string(strip_tags(stripslashes(trim($datos["emailEnlace"]))));
+		$telefonoEnlace = mysql_real_escape_string(strip_tags(stripslashes(trim($datos["telefonoEnlace"]))));
+		$nomEvento = mysql_real_escape_string(strip_tags(stripslashes(trim($datos["evento"]))));
+		$domicilio = mysql_real_escape_string(strip_tags(stripslashes(trim($datos["domicilio"]))));
+		$colonia = mysql_real_escape_string(strip_tags(stripslashes(trim($datos["colonia"]))));
+		$delegacion = mysql_real_escape_string(strip_tags(stripslashes(trim($datos["delegacion"]))));
+		$seccion = mysql_real_escape_string(strip_tags(stripslashes(trim($datos["seccion"]))));
+		$dependencia = mysql_real_escape_string(strip_tags(stripslashes(trim($datos["dependencia"]))));
+		$dependenciaMunicipal = mysql_real_escape_string(strip_tags(stripslashes(trim($datos["dependenciaMunicipal"]))));
+		$nPreGira = $this->nPreGira();
+		$consulta = "";
+		// mysql_query($consulta,$this->conexion) or die (mysql_error());
+		// $consulta = "SELECT LAST_INSERT_ID() AS id;";
+		// $resultado = mysql_query($consulta,$this->conexion) or die (mysql_error());
+		// $preGiraId = mysql_fetch_assoc($resultado);
+		// $preGiraId = $preGiraId["id"];
+	}
+	public function nPreGira()
+	{
+		$consulta = "SELECT (COALESCE(MAX(ngira),0)+1) AS npregira FROM pregira";
+		$resultado = mysql_query($consulta,$this->conexion) or die (mysql_error());
+		$ngira = mysql_fetch_assoc($resultado);
+		return $ngira["npregira"];
 	}
 }
